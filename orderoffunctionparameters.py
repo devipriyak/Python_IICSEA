750f94df5c7e4f983666babe65837a42b0e006e9
devipriyak <k.devipriya20@gmail.com>
2021-06-24 14:23:38 +0530

    Add files via upload

---
+++ orderoffunctionparameters.py
@@ -0,0 +1,21 @@
+def display(name,age):
+      print "Name is: " ,name
+      print "Age is: " ,age
+#call the display function
+display("Ravi",20)
+display("Raj",21)
+#call the display function
+
+'''display(21,"Rahul")#is it acceptable or not?
+name is 21
+Age is Rahul
+'''
+#Passing values along with key(Variable Names)
+display(age=21,name="Rahul")
+display(name="Raghu",age=19)
+'''
+Name is:  Rahul
+Age is:  21
+'''
+
+      
